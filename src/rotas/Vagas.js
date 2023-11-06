468dad7aef536dc7322321a63f318ceed1b1eeae
DIOGOBRODRIGUES <diogo.borbar@gmail.com>
2023-11-06 20:18:22 -0300

    aula 06/11

---
+++ src/rotas/Vagas.js
@@ -0,0 +1,12 @@
+import Intro from "../componentes/Intro"
+import Pesquisa from "../componentes/Pesquisa"
+
+export default function Vagas (){
+    return(
+        <div className="App">
+            <Intro/>
+            <Pesquisa/>
+        </div>
+
+    );
+}
